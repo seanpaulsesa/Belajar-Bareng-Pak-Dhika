f3d4a38c8c8b84d4b9cc754d4c05e9ecbf0343dd
Paulus Sesa <pauluspaul7700@gmail.com>
2023-11-20 19:13:42 +0900

    Readme

---
+++ pertemuan2/index.php
@@ -0,0 +1,67 @@
+<?php 
+// pertemuan 2-php dasar
+// sintaks php
+
+// standar Output
+// echo,print
+// print_r
+// var_dump
+
+// penulisan sintaks php
+// 1.php di dalam html
+// 2.html di dalam php
+
+// variabel dan tipe data
+// variabel
+// tidak boleh di awali dengan angka, tapi boleh mengandung angka
+// $nama = "paulus sesa";
+// echo 'halo, nama saya $nama';
+
+// operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// pengabungan streing / concatenation / concat
+// .
+// $nama_depan = "paulus";
+// $nama_belakang = "sesa";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+
+// $nama = "paulus";
+// $nama .= " ";
+// $nama .= "sesa";
+// echo $nama;
+
+// operator perbandingan
+// <, >, <=, >=, ==
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1")
+
+// operator Logika
+// &&, ||, !
+echo 'PAULUS SESA'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Belajar PHP Bersama Pak Sandhika Galih</title>
+</head>
+<body>
+    <h1>Hello, Selamat Datang <?php echo "Pace Koding"; ?></h1>
+</body>
+</html>
